a4727962bc343f5fd6cde57f8a13fcd75ff3f85f
manisha-27 <67019423+manisha-27@users.noreply.github.com>
2021-05-28 23:59:03 +0530

    Add files via upload

---
+++ Sparks_Foundation/index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- adding -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- added -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="index.css">
+    <title>Matru Bank</title>
+</head>
+
+<body>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background:linear-gradient(to right, #000000,transparent)">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li style="color: #ffffff; font-size: 30px;">Matru Bank</li>
+                </ul>
+                <form class="d-flex" align="right">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html" style="color: #000000;">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="createuser.php" style="color: #000000;">Create User</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="transfermoney.php" style="color: #000000;">Transfer Money</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="transactionhistory.php" style="color: #000000;">Transaction History</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html" style="color: #000000;">Contact Us</a>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+    <div class="container div2" align="Left" style="color: #ffffff;">
+        <h2>Matru Bank</h2>
+        <h3>Welcomes You</h3>
+        <br>
+        <div class="container info">
+            <div class="row text-justify">
+                <div class="col-md-4">
+                    <div class="container div1 info">
+                        <div class="row text-justify">
+                            <div class="col">
+                                <br>
+                                <h4><a href="createuser.php"><button>Create a User</button></a></h4>
+                                <br><br>
+                                <h4><a href="transfermoney.php"><button>Make a Transaction</button></a></h4>
+                                <br><br>
+                                <h4><a href="transactionhistory.php"><button>Transaction History</button></a></h4>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <img src="https://pngimage.net/wp-content/uploads/2018/06/ideias-png-6.png" alt="" width="70%" height="100%" align="Center"> -->
+                </div>
+                <div class="col-md-8" align="right">
+                    <img src="https://2xz6op2vaxk28i6yk1df4rp1-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/Colombo_2-copy-4-e1575542263596.png"
+                        alt="" width="60%" height="100%" align="Center">
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <br>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+
+</html>
